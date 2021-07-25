c7a68eb7924bf4905a4da1b87fefb60662ae11f3
dblom <d.blom4865@gmail.com>
2021-07-25 00:20:50 -0500

    fixed scaling issue
    corrected RimWorld_MainTabWindow_InitialSize classname
    
    updated README.md to include patches for MainTabWindow.InitialSize
    
    removed unused postfix parameter
    
    1

--- 1.3/Source/Data/State.cs
+++ 1.3/Source/Data/State.cs
@@ -7,14 +7,15 @@ namespace RimHUD.Data
     internal static class State
     {
         public static bool Activated { get; set; } = true;
-        public static bool ModifyPane => ShowPane && Theme.InspectPaneTabModify.Value;
+        public static bool ModifyPane => RecalculateSize || ShowPane && Theme.InspectPaneTabModify.Value;
         public static bool CompressLetters => Active && Theme.LetterCompress.Value;
         public static bool HudFloatingVisible => !Theme.HudDocked.Value && ShowPane;
 
         public static Pawn SelectedPawn => GetSelectedPawn();
 
-        private static bool Active => Activated && Current.ProgramState == ProgramState.Playing;
+        public static bool Active => Activated && Current.ProgramState == ProgramState.Playing;
         private static bool ShowPane => Active && MainButtonDefOf.Inspect.TabWindow.IsOpen && SelectedPawn != null;
+        public static bool RecalculateSize { get; set; }
 
         private static Pawn GetSelectedPawn()
         {
